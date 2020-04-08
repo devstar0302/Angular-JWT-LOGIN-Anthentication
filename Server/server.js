c67fd2578e532fc61fb6fc64b7e1513b41ac16d9
unknown <rudyking0302@gmail.com>
2020-04-08 16:09:05 +0800

    My works

---
+++ Server/server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const dbConfig = require('./app/config/db.config')
+
+var corsOptions = {
+    origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if(!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if(err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
+
+
+//parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+//parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Web coder Application" });
+});
+
+
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+}); 
